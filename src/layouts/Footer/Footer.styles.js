7d98242aa275c90e48ed1e23b60df51512910150
Chris-Yeo <chrisyeo.tr@gmail.com>
2020-09-18 02:08:39 +0700

    api sementara, belum 100% responsif, belum add function

--- src/layouts/Footer/Footer.styles.js
+++ src/layouts/Footer/Footer.styles.js
@@ -1,5 +1,58 @@
 import { css } from '@emotion/core';
 
 export const wrapperStyles = css`
-  padding: 0px 50px;
+  padding: 50px 50px;
+  margin-top: 200px;
+  display: flex;
+  background-color: #209516;
 `;
+
+export const imageLogo = css `
+width: 150px;
+filter: brightness(0) invert(1);
+margin-right: 160px;
+margin-left: 20px;
+`
+
+export const footer = css `
+color: #FFF;
+font-size: 14px;
+line-height: 1;
+margin-right: 160px;
+`
+
+export const footerTitle = css `
+color: #FFF;
+font-weight: 600;`
+
+export const subsLogo = css `
+width: 70px;
+filter: brightness(0) invert(1);
+margin-bottom: 30px;
+`
+
+export const subscription = css `
+margin-left: 100px;
+text-align: center;
+color: #FFF;`
+
+export const wrapperStyles2 = css `
+display: flex;
+background-color: #12520C;
+padding-top: 20px;
+justify-content: space-between;
+color: #FFF;
+font-size: 13px;
+`
+
+export const mini = css `
+padding-left: 50px;
+`
+
+export const icons = css `
+display: flex;
+margin-right: 50px;`
+
+export const icon = css `
+margin-left: 15px;
+`
