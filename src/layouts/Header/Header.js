3bfad05bc4c0e055fef72d3fa0beccaad1b25a8b
miftah faris <miftah.faris07@yahoo.co.id>
2020-09-21 15:29:12 +0700

    fix responsive layout

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,54 +1,74 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/core';
-import { Dropdown } from 'react-bootstrap';
+import React from "react";
+import { jsx } from "@emotion/core";
+import { Dropdown } from "react-bootstrap";
 
-import Logo from '../../assets/logo-mini-menagerie.png';
+import Logo from "../../assets/logo-mini-menagerie.png";
 import {
-  wrapperStyles,
-  imageStyles,
-  breedsShop,
-  shopButton,
-  navbar1,
-  breedsButton,
-  formSearch,
-  searchText,
-  searchButton,
-  cart,
-} from './Header.styles';
-import { Button } from 'react-bootstrap'
-
+    imageStyles,
+    breedsButton,
+    formSearch,
+    searchText,
+    searchButton,
+    cart,
+    shopButton,
+} from "./Header.styles";
+import { Button, Navbar, Nav } from "react-bootstrap";
 
 const Header = () => {
-  return (
-    <div css={wrapperStyles}>
-      <div css={navbar1}>
-        <img src={Logo} css={imageStyles} alt="Mini Menagerie" />
-        <div css={breedsShop}>
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic" css={breedsButton}>
-            Breeds
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Dog Breeds</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Cat Breeds</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <div css={shopButton}><a href='/shop'>Shop</a></div>
-      </div>
-      </div>
-      <div>
-        <form css={formSearch}>
-          <input type="text" css={searchText} placeholder="Search your future best friend"></input>
-          <button type="submit" css={searchButton}><i class="fas fa-search"></i></button>
-          <button type="submit" css={cart}><i class="fas fa-shopping-cart"></i></button>
-       </form>
-      </div>
-      <div>
-        <Button variant="light">Sign Up</Button>
-        <Button variant="light">Log In</Button>
-      </div>
-    </div>
-  );
-}
+    return (
+        <React.Fragment>
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">
+                    <img src={Logo} css={imageStyles} alt="Mini Menagerie" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Dropdown>
+                            <Dropdown.Toggle
+                                variant="success"
+                                id="dropdown-basic"
+                                css={breedsButton}
+                            >
+                                Breeds
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">
+                                    Dog Breeds
+                                </Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">
+                                    Cat Breeds
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <div css={shopButton}>
+                            <a href="/shop">Shop</a>
+                        </div>
+                    </Nav>
+                    <div>
+                        <form css={formSearch}>
+                            <input
+                                type="text"
+                                css={searchText}
+                                placeholder="Search your future best friend"
+                            ></input>
+                            <button type="submit" css={searchButton}>
+                                <i class="fas fa-search"></i>
+                            </button>
+                            <button type="submit" css={cart}>
+                                <i class="fas fa-shopping-cart"></i>
+                            </button>
+                        </form>
+                    </div>
+                    <div>
+                        <Button variant="light">Sign Up</Button>
+                        <Button variant="light">Log In</Button>
+                    </div>
+                </Navbar.Collapse>
+            </Navbar>
+        </React.Fragment>
+    );
+};
 
 export default Header;
