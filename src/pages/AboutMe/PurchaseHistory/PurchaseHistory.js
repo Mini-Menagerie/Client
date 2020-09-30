b0683c9d8d6b7de462647be9f08b2a40374b3340
Angga Prasetya W <agpw.id@gmail.com>
2020-09-30 15:41:03 +0800

    shopping cart

--- src/pages/AboutMe/PurchaseHistory/PurchaseHistory.js
+++ src/pages/AboutMe/PurchaseHistory/PurchaseHistory.js
@@ -1,16 +1,15 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
-import { Card, Col, Row, Container, Accordion } from "react-bootstrap";
+import { Card, Col, Row, Container, Accordion, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import format from 'date-fns/format'
 import axios from '../../../helpers/axios';
 
 import ColoredLine from '../../../components/ColoredLine'
-import { head, listInfo, mainOne, dated, desc } from "./PurchaseHistory.styles";
+import { head } from "./PurchaseHistory.styles";
 
 const PurchaseHistory = () => {
     const [purchaseHistory, setPurchaseHistory] = useState([]);
-    // const [, setErrorMessage] = useState();
 
     const getTransactionDetails = async () => {
         const user = JSON.parse(localStorage.getItem("user"));
@@ -23,39 +22,28 @@ const PurchaseHistory = () => {
         getTransactionDetails()
     }, [])
 
-    // let date = '2020-09-29T08:16:26.224Z'
-    let parseXdate = format(new Date('2020-09-29T08:16:26.224Z'), 'dd MMMM yyyy')
-    console.log(parseXdate);
-
+    console.log(purchaseHistory);
 
     return (
         <Container>
             <Row css={head}>
                 <h2>History Purchase</h2>
             </Row>
-            {/* <Row>
-                <Accordion style={{ width: '100%' }}>
-                    <Card>
-                        <Accordion.Toggle as={Card.Header} eventKey="0">123</Accordion.Toggle>
-                        <Accordion.Collapse eventKey="0">
-                            <Card.Body>Hello! I'm the body</Card.Body>
-                        </Accordion.Collapse>
-                    </Card>
-                </Accordion>
-            </Row> */}
 
             <Row >
                 {purchaseHistory.map(value => (
                     <Accordion style={{ width: '100%', marginBottom: '10px' }}>
                         <Card>
-                            <Row style={{ display: 'flex' }}>
-                                <Col style={{ paddingRight: '0px' }}>
-                                    <Accordion.Toggle as={Card.Header} eventKey="0" style={{ width: '100%' }}>{value.idTransaction !== null && value.idTransaction !== undefined && value.idTransaction._id}</Accordion.Toggle>
-                                </Col>
-                                <Col style={{ textAlign: 'right', paddingLeft: '0px' }}>
-                                    <Accordion.Toggle as={Card.Header} eventKey="0" style={{ width: '100%' }}>{format(new Date(value.idTransaction !== null && value.idTransaction !== undefined && value.idTransaction.createdAt), 'HH:mm, dd MMMM yyyy')}</Accordion.Toggle>
-                                </Col>
-                            </Row>
+                            <OverlayTrigger placement={'left'} overlay={<Tooltip id="tooltip-disabled">Click for transaction details</Tooltip>}>
+                                <Row style={{ display: 'flex' }}>
+                                    <Col style={{ paddingRight: '0px' }}>
+                                        <Accordion.Toggle as={Card.Header} eventKey="0" style={{ width: '100%' }}>{value.idTransaction !== null && value.idTransaction !== undefined && value.idTransaction._id}</Accordion.Toggle>
+                                    </Col>
+                                    <Col style={{ textAlign: 'right', paddingLeft: '0px' }}>
+                                        <Accordion.Toggle as={Card.Header} eventKey="0" style={{ width: '100%' }}>{format(new Date(value.idTransaction !== null && value.idTransaction !== undefined && value.idTransaction.createdAt), 'HH:mm, dd MMMM yyyy')}</Accordion.Toggle>
+                                    </Col>
+                                </Row>
+                            </OverlayTrigger>
                             <Accordion.Collapse eventKey="0">
                                 <Card.Body>
                                     {value.idProduct.map(value => (
@@ -83,36 +71,6 @@ const PurchaseHistory = () => {
                     </Accordion>
                 ))}
             </Row>
-
-            {/* <div css={listInfo}>
-                {purchaseHistory.map((value) => (
-                    <div key={value._id}>
-                        <Card>
-                            <Card.Body>
-                                <div css={dated}>
-                                    <span>{value.createdAt}</span>
-                                </div>
-                                <Row>
-                                    <Col xs={6} css={mainOne}>
-                                        <img src={value.idProduct.image !== undefined && value.idProduct.image[0].image} alt="product" />
-                                    </Col>
-                                    <Col css={desc}>
-                                        <div>
-                                            <b>{value.idProduct.productName}</b> -{" "}
-                                            <span>{value.idProduct.categories}</span>
-                                            <p>
-                                                IDR
-                                                {value.idTransaction !== null && value.idTransaction !== undefined && value.idTransaction.totalPrice}
-                                            </p>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </Card.Body>
-                        </Card>
-                        <br />
-                    </div>
-                ))}
-            </div> */}
         </Container>
     );
 };
